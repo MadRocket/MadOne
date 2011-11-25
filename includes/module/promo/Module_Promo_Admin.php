adffac1cc80c89aeeff1c72b105f9702bb36f72e
Dmitriy Rozhkov <rojkov.dmitry@gmail.com>
2011-11-25 23:18:29 +0700

    Promo Module Bugfix

--- includes/module/promo/Module_Promo_Admin.php
+++ includes/module/promo/Module_Promo_Admin.php
@@ -4,17 +4,28 @@
  * @author \$Author$
  */
  
-class Module_Promo_Admin extends Madone_Module {
+class Module_Promo_Admin extends AbstractModule {
     function handleHtmlRequest($uri) {
-        if(!empty($_POST['submit'])) {
-            $promo = PromoImages()->create($_POST);
-            $promo->image = empty($_FILES['image']) ? null : $_FILES['image'];
-            $promo->save();
+        if(array_key_exists('submit', $_POST)) {
+            if(array_key_exists('id', $_POST)) {
+                if( $_FILES['image']['name'] ) {
+                    $_POST['image'] = $_FILES['image'];
+                }
+
+                $staff = PromoImages()->get($_POST['id']);
+                $staff->copyFrom($_POST);
+                $staff->save();
+            }
+            else {
+                $_POST['image'] = $_FILES['image'];
+                $staff = PromoImages()->create($_POST);
+            }
+
+            header("Location: {$this->uri}");
+            return true;
         }
 
         $items = PromoImages()->order('position')->all();
         return $this->getTemplate('index.twig', array('items' => $items));
     }
 }
-
-?>
