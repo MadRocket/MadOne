18706834519ca7fcb7bfe30e4c9b0f589088ce02
Dmitriy Rozhkov <rojkov.dmitry@gmail.com>
2012-01-06 01:46:28 +0700

    Http foundation Request and Response
    also removed unused show404(), postprocess() methods

--- includes/Madone/Core.php
+++ includes/Madone/Core.php
@@ -1,20 +1,38 @@
-<?
-
+<?php
 /**
  * Ядро системы управления, отображающее сайт.
  */
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
 class Madone_Core {
     protected static $languages = array();
     protected static $language = null;
     protected static $langRegExp = null;
     protected static $developmentMode = true;
+
+    /**
+     * @var Symfony\Component\HttpFoundation\Request
+     */
+    protected static $request = null;
+
+    /**
+     * @var Symfony\Component\HttpFoundation\Response
+     */
+    protected static $response = null;
+
     /**
      *	Инициализация класса
      */
 	static function _init() {
 		// Определим нужно ли влючать режим разработки
 		self::detectDevelompentMode();
-	
+
+        self::$request = Request::createFromGlobals();
+
+        self::$response = new Response( );
+
 		// На инициализацию устанавливаем собственные обработчики ошибок и исключений
 		self::setErrorHandlers();
 		
@@ -27,7 +45,7 @@ class Madone_Core {
     	}
     	
     	// Если режим разработки — синхронизируем модели
-		if( self::$developmentMode ) {
+		if( self::getDevelopmentMode() ) {
 			Storm_Core::sync();
 		}
 	}
@@ -36,12 +54,12 @@ class Madone_Core {
 	 *	Определение режима разработки
 	 */
 	static function detectDevelompentMode() {
-		if( ! is_bool( self::$developmentMode ) ) {
+		if( ! is_bool( self::getDevelopmentMode() ) ) {
 			self::$developmentMode = $_SERVER['SERVER_ADDR'] === '127.0.0.1' ? true : false;
 		}
 	}
 	
-	static function developmentMode() {
+	static function getDevelopmentMode() {
 		return self::$developmentMode;
 	}
 	
@@ -93,20 +111,30 @@ class Madone_Core {
 		// Устанавливаем внутреннюю кодировку мультибайтовых функций
 		mb_internal_encoding( Storm_Core::getLanguage()->getCharset() );
 	}
-	
+
+
+    public static function getRequest() {
+        return self::$request;
+    }
+
+    public static function getResponse() {
+        return self::$response;
+    }
+
     /**
      *	Обработка HTTP запроса к сайту.
      *	Основное метод, через который работают все страницы.
      *	Не возвращает ничего, текст сайта отправляется на стандартный вывод.
      */
-    static function run() {
+    public static function run() {
         self::_init();
 
 		// Тут будут условия фильтрации страниц
 		$filter = null;
 		
 		// Получаем текущий URI, выделяем из него имена каталогов/файлов
-		$names = Madone_Utilites::getUriPathNames();
+        $path = self::getRequest()->getPathInfo();
+		$names = Madone_Utilites::getUriPathNames($path);
 		/*
 		Если есть имена - пытаемся выбрать одну из внутренних страниц сайта
 		Например, условия выборки для страницы /news/archive/2008 получатся такие:
@@ -126,18 +154,6 @@ class Madone_Core {
 				$q = Q( array( 'uri' => $uri ) );
 				$filter = $filter ? QOR( $filter, $q ) : $q;
 			}
-			
-			// Фильтр вложенных страниц приложения
-//			$filter = QAND( $filter, Q( array( 'type__has_subpages' => true ) ) );
-			
-			// Фильтр полного совпадения uri страниц без вложенных страниц приложения
-//			$filter = QOR(
-//                $filter,
-//                QAND(
-//                    Q( array( 'uri' => $uri ) ),
-//                    Q( array( 'type__has_subpages' => false ) )
-//                )
-//            );
 		}
 		// Путь пуст, выбираем главную страницу
 		else {
@@ -152,14 +168,17 @@ class Madone_Core {
 			$app_uri = mb_substr( $uri, mb_strlen( $p->uri, 'utf-8' ), mb_strlen( $uri, 'utf-8' ), 'utf-8' );
 			
 			// Запускаем приложение, соответствующее модулю, если оно отработало — завершаем работу
-            $response = null;
+            $app_response = null;
             $app_classname = "Madone_Module_{$p->module}_Application";
             if(class_exists($app_classname)) {
                 $app = new $app_classname();
-                $response = $app->run($p, $app_uri);
+                $app_response = $app->run($p, $app_uri);
             }
-            if($response) {
-				print( self::postprocess( ob_get_clean() ) );
+            if($app_response) {
+                $content = ob_get_clean();
+                self::getResponse()->setContent( $content );
+                self::getResponse()->send();
+
 				return;
 			}
 			// продолжаем обработку среди всех выбранных приложений, попадающих в этот же uri
@@ -167,25 +186,9 @@ class Madone_Core {
 		ob_end_clean();
 
 		// Не сработало ни одно приложение — ничего не найдено
-		Madone_Core::show404();
-    }
-
-    /**
-     * Завершение выполнения скрипта с кодом 404. Мимикрия под Apache.
-     * @static
-     * @param null $uri
-     * @return mixed
-     */
-    static function show404( $uri = null ) {
-        if( is_null( $uri ) ) {
-            $uri = $_SERVER['REQUEST_URI'];
-        }
-    
-        header( "{$_SERVER['SERVER_PROTOCOL']} 404 Not Found", true, 404 );
-
-        print self::twig()->render('404.twig', array('uri' => $uri));
-        
-        return null;
+        self::getResponse()->setContent( self::twig()->render('404.twig', array('uri' => $uri)) );
+        self::getResponse()->setStatusCode(404, "Страница не найдена");
+        self::getResponse()->send();
     }
 
     /**
@@ -204,7 +207,7 @@ class Madone_Core {
 		if( ! headers_sent() ) {
 			header( "Content-Type: text/html;charset=utf-8" );
 		}
-		if( self::$developmentMode ) {
+		if( self::getDevelopmentMode() ) {
 			if( strstr( $errstr, '<html>' ) === false ) {
 				echo "<html><h2>" . $errstr . "</h2><h4>" . Madone_Utilites::getPhpErrorName( $errno ) ." in {$errfile} on line {$errline}.</h4>";
 				$trace = debug_backtrace(); array_shift( $trace );
@@ -226,7 +229,7 @@ class Madone_Core {
     /**
      * Обработчик неперехваченных исключений
      * @static
-     * @param $exception
+     * @param Exception $exception
      */
 	static function uncaught_exception_handler( $exception ) {
 		// Сбрасываем все буферы вывода, которые есть
@@ -237,7 +240,7 @@ class Madone_Core {
 			header( "Content-Type: text/html;charset=utf-8" );
 		}
 		
-		if( self::$developmentMode ) {
+		if( self::getDevelopmentMode() ) {
 			echo "<html><h2>" . $exception->getMessage() . "</h2><h4>" . get_class( $exception ) ." in " . $exception->getFile() . " on line " . $exception->getLine() . ".</h4>";
 			echo Madone_Utilites::formatDebugTrace( $exception->getTrace() );
 			echo "</html>";
@@ -248,24 +251,6 @@ class Madone_Core {
 		exit;
 	}
 
-    /**
-     * Постобработка кода страницы
-     * @static
-     * @param $text
-     * @return mixed
-     */
-	static function postprocess( $text ) {
-		// Добавляем lang-атрибут для текущего языка
-		$text = str_replace( '<html', '<html lang="'. Storm_Core::getLanguage()->getName() .'"', $text );
-		
-// FIXME: i18n support Если включен язык, отличный от default — переписываем все ссылки кроме static
-//		if( self::$language ) {
-//			$text = preg_replace( '~("/)((?!static|'.self::$langRegExp.')[^"]*")~S', '$1'.self::$language.'/$2', $text );
-//		}
-		
-		return $text;
-	}
-	
 	static function getLanguages() {
 		return preg_grep( '/default/', array_keys( self::$languages ), PREG_GREP_INVERT );
 	}
@@ -287,7 +272,6 @@ class Madone_Core {
         $path = is_array($path) ? $path : array($path);
         $twig = Outer_Twig::get(array_merge($path, array("{$_SERVER['DOCUMENT_ROOT']}/includes/template/_default", "{$_SERVER['DOCUMENT_ROOT']}/includes/template")));
         $twig->addGlobal('config', Madone_Config::getInstance());
-        $twig->addGlobal('server', Madone_Utilites::server());
 
         $twig->addGlobal('madone_image_helper', new Madone_Helper_Image());
         $twig->addGlobal( 'madone', new Madone_Core() );
