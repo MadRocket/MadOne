08cef3675db45df4f49a42b539572938903d14bf
Dmitriy Rozhkov <rojkov.dmitry@gmail.com>
2012-01-07 21:47:37 +0700

    Outer_Email -> Madone_Email

---
+++ includes/Madone/Email.php
@@ -0,0 +1,27 @@
+<?php
+class Madone_Email {
+
+    static function send($subject, $from, $to, $body) {
+
+        if (! $from) {
+            $from = array( Madone_Config::getInstance()->{'site_title'} => "noreply@{$_SERVER['HTTP_HOST']}" );
+        }
+
+        $transport = Swift_MailTransport::newInstance();
+
+        // Create the Mailer using your created Transport
+        $mailer = Swift_Mailer::newInstance($transport);
+
+        // Create a message
+        $message = Swift_Message::newInstance($subject)
+          ->setFrom($from)
+          ->setTo($to)
+          ->setBody($body)
+          ;
+
+        // Send the message
+        $result = $mailer->send($message);
+
+        return $result;
+    }
+}
