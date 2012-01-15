feb26582fcd859c2ce9c82e219655656409ed4a7
Dmitriy Rozhkov <rojkov.dmitry@gmail.com>
2012-01-15 19:01:55 +0700

    Delete own autoload.php now using only composer's autoload

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 error_reporting( E_ALL );
 date_default_timezone_set('Asia/Krasnoyarsk');
 
-require_once( __DIR__."/autoload.php" );
+require __DIR__ . '/vendor/.composer/autoload.php';
 
 $madone = new Madone_Core();
 
